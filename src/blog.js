93543eef3c59ef8587d39c3c63644eb869c72fcf
Victor Locoman <victor@locoman.ro>
2020-10-07 21:19:32 +0300

    Implementet Blog single and homepage

--- src/blog.js
+++ src/blog.js
@@ -3,9 +3,18 @@ const db = require('./db');
 
 const router = express.Router();
 
-// @todo: completează cu toate rutele necesare
 router.get('/', (req, res) => {
     res.render('pages/home', {posts: db.getAll('posts')});
+});
+
+router.get('/posts/:slug', (req, res) => {
+    const slug = req.params.slug;
+    const post = db.getBy('posts', 'slug', slug);
+    if (post) {
+        res.render('pages/single', {post});
+    } else {
+        res.render('pages/notFound', {});
+    }
 });
 
 module.exports = router;
