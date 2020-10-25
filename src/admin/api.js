dbe97eafcbbbba6deba55aaf59bca705adf759d5
Victor Locoman <victor@locoman.ro>
2020-10-25 17:58:05 +0200

    JSON based DB example

--- src/admin/api.js
+++ src/admin/api.js
@@ -1,9 +1,10 @@
-const express = require("express");
+const express = require('express');
+const db = require('../db');
 
 const router = express.Router();
 
-router.get("/posts", (req, res) => {
-  res.send(db.getAll("posts"));
+router.get('/posts', async (req, res) => {
+  res.send(await db.getAll('posts'));
 });
 // @todo: de adăugat toate rutele necesare pentru a avea un API restfull
 
--- src/db.js
+++ src/db.js
@@ -1,31 +1,50 @@
-// vom stoca datele în memorie, prin obiectul local data
-// pentru a interacționa cu acest obiect, vom crea metodele CRUD
-const data = {
+const fs = require('fs').promises;
+
+let data = {
   posts: [],
 };
+
+const dbFile = 'data.my-json';
+fs.readFile(dbFile, { encoding: 'utf-8' })
+  .then(content => {
+    console.log('content', content);
+    if (content) {
+      data = JSON.parse(content);
+    }
+  })
+  .catch(error => {
+    // no file, keep default data
+  });
+
 let counter = 0;
+
+const persistOnFile = () => {
+  fs.writeFile(dbFile, JSON.stringify(data)).catch(error => {
+    console.log('error on write', error);
+    // do nothing, I don't care
+  });
+};
 
 const add = (table, item) => {
   // create
   item.id = counter++;
   data[table].push(item);
+  persistOnFile();
   return item;
 };
 
 const getBy = (table, key, value) => {
   // read by key
-  const item = data[table].find((dataItem) => dataItem[key] === value);
+  const item = data[table].find(dataItem => dataItem[key] === value);
   return item;
 };
 const get = (table, id) => {
   // read
-  return getBy(table, "id", id);
+  return getBy(table, 'id', id);
 };
 const set = (table, updatedItem) => {
   // update
-  const itemIndex = data[table].findIndex(
-    (dataItem) => dataItem.id === updatedItem.id
-  );
+  const itemIndex = data[table].findIndex(dataItem => dataItem.id === updatedItem.id);
   if (itemIndex === -1) {
     if (counter <= updatedItemid) {
       counter = updatedItemid + 1;
@@ -34,20 +53,23 @@ const set = (table, updatedItem) => {
     return updatedItem;
   }
   const item = data[table][itemIndex];
+  persistOnFile();
   return item;
 };
 const remove = (table, id) => {
   // delete
-  const itemIndex = data[table].findIndex((dataItem) => dataItem.id === id);
-  delete data[table][itemIndex];
+  const itemIndex = data[table].findIndex(dataItem => dataItem.id === id);
+  delete data[table][itemIndex]; // @todo: fix this with .splice(...)
+  persistOnFile();
   return itemIndex === -1 ? false : true;
 };
 
-const getAll = (table) => {
+const getAll = table => {
   return data[table].slice(); // întoarcem o copie al array-ului, ca să nu-l modifice cineva întâmplător
 };
-const removeAll = (table) => {
+const removeAll = table => {
   data[table] = [];
+  persistOnFile();
 };
 
 module.exports = { add, getBy, get, set, remove, getAll, removeAll };
