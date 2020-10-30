ff174b408ba39977213cc6a2edb1122ec4e1ecd0
Marian Puie <60893978+PuieMarian@users.noreply.github.com>
2020-10-30 14:21:16 +0200

    Update db.js

--- src/db.js
+++ src/db.js
@@ -28,3 +28,38 @@ const removeAll = (table) => {
 };
 
 module.exports = {add, get, set, remove, getAll, removeAll};
+
+
+const MongoClient = require('mongodb').MongoClient;
+const populate = require('./populateDb');
+
+const getMongoUri = () => {
+    // Datele din process.env sunt cele din fișierul .env, dacă nu există, trebuie creat după modelul .env.example
+    const user = encodeURIComponent(process.env.mongoUser);
+    const pass = encodeURIComponent(process.env.mongoPass);
+    const url = process.env.mongoUrl;
+    return `mongodb+srv://${user}:${pass}@${url}/`;
+};
+const client = new MongoClient(getMongoUri(), { useUnifiedTopology: true });
+module.exports = { client };
+
+client
+    .connect()
+    .then(() => console.log('MongoDB connected'))
+    .catch(error => console.error('Error on connecting to MongoDB server', error))
+    .then(() => {
+        const posts = client.db('app').collection('posts');
+        module.exports.posts = posts;
+        populate(posts);
+    });
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://mighel1998:mighel1998@cluster0.qbak3.mongodb.net/app?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log(await posts.find({}).limit(1).forEach(console.log))
+    // perform actions on the collection object
+    client.close();
+});
