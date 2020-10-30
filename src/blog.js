4dcd3d00884c19b8ad3bec2d18484cf97c2d6d4c
Marian Puie <60893978+PuieMarian@users.noreply.github.com>
2020-10-30 14:19:15 +0200

    Update blog.js

--- src/blog.js
+++ src/blog.js
@@ -3,9 +3,39 @@ const db = require('./db');
 
 const router = express.Router();
 
-// @todo: completează cu toate rutele necesare
+
+router.get('/', async (req, res) => {
+    const posts = await db.posts.find().toArray();
+    res.render('pages/home', { posts });
+});
+
+
 router.get('/', (req, res) => {
-    res.render('pages/home', {posts: db.getAll('posts')});
+    const getPosts = { posts: db.getAll('posts') };
+    res.render('pages/home', getPosts);
+});
+
+router.get('/:id', (req, res) => {
+    const id = +req.params.id;
+    const responseItem = db.get('posts', id);
+    if (responseItem.status) {
+        return res.status(responseItem.status).send(responseItem.msg);
+    }
+    const post = { posts: [responseItem] };
+
+    res.render('pages/home', post);
+});
+
+
+router.get('/posts/:slug', async (req, res) => {
+    const slug = req.params.slug;
+    const post = await db.posts.findOne({ slug });
+    console.log(post);
+    if (post) {
+        res.render('pages/single', { post });
+    } else {
+        res.render('pages/notFound', {});
+    }
 });
 
 module.exports = router;
